c253227614923c6291be671d988385dd4911d84a
xj <xu5621818@126.com>
2018-10-14 16:55:46 +0800

    xxx

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -4,7 +4,9 @@
       <a href=""><img src="../../assets/images/logo2x.png" /></a>
     </div>
     <div class="tab">
-      <a :href="item.herf" v-for="item in tablist" :class="{hover:item.select}">{{item.name}}</a>
+      <router-link :to="{name:item.herf}" v-for="item in tablist">
+        <span :class="{hover:item.select}">{{item.name}}</span>
+      </router-link>
     </div>
     <div class="clear"></div>
   </div>
@@ -24,10 +26,10 @@ export default {
   data(){
     return{
       tablist:[
-        {name:'首页',herf:'/',select:false},
-        {name:'产品',herf:'/',select:false},
-        {name:'商务合作',herf:'/',select:false},
-        {name:'关于我们',herf:'/about',select:false},
+        {name:'首页',herf:'home',select:false},
+        {name:'产品',herf:'home',select:false},
+        {name:'商务合作',herf:'home',select:false},
+        {name:'关于我们',herf:'about',select:false},
       ]
     }
   },
@@ -43,14 +45,14 @@ export default {
 .header{position: absolute;width: 100%;z-index: 2;}
 .header .logo{float: left;margin-left: 100px;margin-top: 40px;}
 .header .tab{float: right;margin-right: 100px;margin-top: 50px;}
-.header .tab a{color: #2C2C31;font-size: 16px;margin-left: 40px;font-weight: bold;display: inline-block;position: relative;}
-.header .tab a:hover{color: #E5496E;}
-.header .tab a:hover:after{
+.header .tab span{color: #2C2C31;font-size: 16px;margin-left: 40px;font-weight: bold;display: inline-block;position: relative;cursor: pointer;}
+.header .tab span:hover{color: #E5496E;}
+.header .tab span:hover:after{
   content: '';height: 3px;width: 30px;background: #E5496E;
   position: absolute;bottom: -5px;left: 50%;margin-left: -15px;
 }
-.header .tab a.hover{color: #E5496E;}
-.header .tab a.hover:after{
+.header .tab span.hover{color: #E5496E;}
+.header .tab span.hover:after{
   content: '';height: 3px;width: 30px;background: #E5496E;
   position: absolute;bottom: -5px;left: 50%;margin-left: -15px;
 }
